b3f018d74ee40977552d32ac02341894cdc5c657
jakeogh <github.com@v6y.net>
2020-07-16 03:48:19 -0700

    auto-commit

--- mpvpy/mpvpy.py
+++ mpvpy/mpvpy.py
@@ -57,7 +57,7 @@ def play(media,
                      input_vo_keyboard=True,
                      osc=True,
                      terminal=True,
-                     input_terminal=True
+                     input_terminal=True,
                      video=video)
 
     # self.m = mpv.MPV(vo='x11')
